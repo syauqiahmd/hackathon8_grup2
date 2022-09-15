b1bc22e3948ed9948e89959bd580e71f2283244d
Ahmad Syauqi <syauqiahmd@Ahmads-MacBook-Pro.local>
2022-09-15 15:31:57 +0800

    update data structure

---
+++ js/structureData.js
@@ -0,0 +1,50 @@
+const arrayReview = {
+  junanda : [
+    {
+    id: 1,
+    namaResto: 'rumah makan padang',
+    alamat: 'nama jalannya',
+    rating: [9, 'fgdahfgdkasgf'],
+    star: 0
+    },
+    {
+    id: 2,
+    namaResto: 'rumah  padang',
+    alamat: 'nama jalannya',
+    rating: [9, 'fgdahfgdkasgf'],
+    star: 0
+    }
+  ],
+  nicholas : [
+    {
+    id: 3,
+    namaResto: 'rumah makan ',
+    alamat: 'nama jalannya',
+    rating: [9, 'fgdahfgdkasgf'],
+    star: 0
+    },
+    {
+    id: 4,
+    namaResto: 'rumah  padang',
+    alamat: 'nama jalannya',
+    rating: [9, 'fgdahfgdkasgf'],
+    star: 0
+    }
+  ],
+  siapa : [
+    {
+    id: 3,
+    namaResto: 'rumah makan padang',
+    alamat: 'nama jalannya',
+    rating: [9, 'fgdahfgdkasgf'],
+    star: 0
+    },
+    {
+    id: 4,
+    namaResto: 'rumah  padang',
+    alamat: 'nama jalannya',
+    rating: [9, 'fgdahfgdkasgf'],
+    star: 0
+    }
+  ]
+}
